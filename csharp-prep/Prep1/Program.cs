5dccf12664cc929107686b4c47c55a02ae1b670f
Elroe Woldemichael <elroebro@gmail.com>
2023-01-14 19:42:53 -0700

    Week 1-2 Done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your name? ");
+        string first_name = Console.ReadLine();
+
+        System.Console.WriteLine("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        System.Console.WriteLine($"Your name is: {last_name}, {first_name}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string value = Console.ReadLine();
+        int grade = int.Parse(value);
+
+        string letter;
+        string decision = "Congratulations! You passed the class! ";
+        switch (grade)
+        {
+            case >= 90:
+                letter = "A";
+                break;
+            case >= 80:
+                letter = "B";
+                break;
+            case >= 70:
+                letter = "C";
+                break;
+            case >= 60:
+                letter = "D";
+                break;
+            case <= 60:
+                letter = "C";
+                break;
+        }
+
+       if (grade < 70)
+       {
+            decision = "Please retake this class, You didn't pass! ";
+       }
+
+       Console.WriteLine($"Your grade is: {letter}"); 
+       Console.WriteLine($"{decision}"); 
     }
 }
